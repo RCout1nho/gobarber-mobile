85f2bf2b224b06969f55e6ec614589e8b0bf2aa7
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2020-09-14 07:54:16 -0400

    Icones e style do form ok

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -104,7 +104,7 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleSignIn}>
+            <Form ref={formRef} onSubmit={handleSignIn} style={{ width: '100%' }} >
               <Input
                 autoCorrect={false}
                 autoCapitalize="none"
