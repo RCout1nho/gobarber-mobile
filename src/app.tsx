8fcb6bbc76529b3fc59e4c11c0c06a40bc8b80c8
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2020-10-16 18:18:42 -0400

    Splash screen resolved

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
-// import 'react-native-gesture-handler';
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
+import SplashScreen from 'react-native-splash-screen';
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
+const App: React.FC = () => {
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[]);
+  return(
+    <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <AppProvider>
       <View style={{ backgroundColor: '#312e38', flex: 1 }} >
@@ -17,6 +19,7 @@ const App: React.FC = () => (
       </View>
     </AppProvider>
   </NavigationContainer>
-)
+  )
+}
 
 export default App;
